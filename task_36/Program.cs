3af866050f0985e68a77a631ee0f2992c51a785c
Zarina <abaevazarina@yandex.ru>
2022-09-15 11:51:21 +0300

    added Readme.md

---
+++ task_36/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] GenerateArray(int length)
+{ 
+   int[] myArray = new int[length];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+      myArray[i] = new Random().Next(10, 100);
+    }
+    return myArray;  
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }  
+}
+        
+int sumOddIndexArray(int[] arr)
+{
+  int sum = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+   if(i % 2 != 0)
+     sum = sum + arr[i];
+  }
+  return sum;
+}
+
+var arr = GenerateArray(10);
+PrintArray(arr);
+var result = sumOddIndexArray(arr);
+Console.WriteLine($"-> {result}");
