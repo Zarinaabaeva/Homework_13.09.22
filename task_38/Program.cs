6afb30ba46721bb3004add99a9313600923cb8df
Zarina <abaevazarina@yandex.ru>
2022-09-15 12:21:08 +0300

    task_38 solved

---
+++ task_38/Program.cs
@@ -0,0 +1,31 @@
+﻿double[] GenerateArray(double length)
+{ 
+   double[] myArray = new double[] {63.5, 17.8, 222.9, 12.7, 788.3};
+
+    return myArray;  
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }  
+}
+        
+double maxMinArray(double[] arr)
+{
+ double max = arr.Max();
+ double min = arr.Min();
+ double difference = max - min;
+ return difference;
+}
+
+var arr = GenerateArray(5);
+PrintArray(arr);
+var result = maxMinArray(arr);
+Console.WriteLine($"разница между максимальным и минимальным значением -> {result}");
+
+
+
+
