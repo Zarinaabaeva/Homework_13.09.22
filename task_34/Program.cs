10ab539dceeee38243a6e31a967ca87d128108ab
Zarina <abaevazarina@yandex.ru>
2022-09-15 11:37:28 +0300

    task_34 solved

---
+++ task_34/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] GenerateArray(int length)
+{ 
+   int[] myArray = new int[length];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+      myArray[i] = new Random().Next(100, 999);
+    }
+    return myArray;  
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }  
+}
+        
+int evenArray(int[] arr)
+{
+   int count = 0;
+   for(int i = 0; i < arr.Length; i++)
+   {
+   if(arr[i] % 2 == 0)
+   count++;
+   }
+      return count;
+   }
+   
+var arr = GenerateArray(10);
+PrintArray(arr);
+var result = evenArray(arr);
+Console.WriteLine($"-> {result}");
